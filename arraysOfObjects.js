b20904656579867213db5ca6ef9a1f81f5a78385
Bronson <keliiakimoto@gmail.com>
2017-11-01 13:44:53 -0700

    added report feature

--- arraysOfObjects.js
+++ arraysOfObjects.js
@@ -1,29 +1,41 @@
 'use strict';
 
-const people = [
-  {
-    name: 'John',
-    jobtitle: 'Founder',
-    boss: Null
-  },
-  {
-    name: 'Bob',
-    jobtitle: 'mechanic', 
-    boss: 'John'
-  },
-  {
-    name: 'Lucy',
-    jobtitle: 'teacher',
-    boss: 'John'
-  },
-  {
-    name: 'Jill',
-    jobtitle: 'engineer',
-    boss: 'John'
-  }
-];
+const Emply1 = {
+  name: 'John',
+  jobtitle: 'Founder',
+  boss: null
+};
+
+const Emply2 = {
+  name: 'Bob',
+  jobtitle: 'mechanic',
+  boss: 'John'
+};
+
+const Emply3 = {
+  name: 'Lucy',
+  jobtitle: 'teacher',
+  boss: 'John'
+};
+
+const Emply4 = {
+  name: 'Jill',
+  jobtitle: 'engineer',
+  boss: 'John'
+};
+
+const Employee = [Emply1, Emply2, Emply3, Emply4];
+
+for (let i in Employee) {
+  let boss = Employee[i].boss;
+  let message = '';
+  if (!boss) {
+    message = 'does not report to anybody.';
+  } else {message = `'reports to ${Employee[i].boss}.'`;}
+console.log(`${Employee[i].jobtitle} ${Employee[i].name} ${message}`);
+}
 
 
-people.forEach(person =>
-  console.log(`${person.jobtitle} ${person.name} reports to ${person.boss}.`)
-);
+// Employee.forEach(person =>
+//   console.log(`${person.jobtitle} ${person.name} reports to ${person.boss}.`)
+// );
